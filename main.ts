c333929f49d546509c886a728d7dee1691d90fc9
bestbish <73271467+bestbish@users.noreply.github.com>
2020-10-22 16:51:16 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -315,3 +315,26 @@ mySprite5 = sprites.create(img`
     ........................
 `, SpriteKind.KillerNPC)
 mySprite5.setPosition(601, 201)
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    let Gravity = 0
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . c d . . . . . . . 
+        . . . . . . . c d . . . . . . . 
+        . . . . . . . c d . . . . . . . 
+        . . . . . . . c b . . . . . . . 
+        . . . . . . . f f . . . . . . . 
+        . . . . . . . c 4 . . . . . . . 
+        . . . . . . . f f . . . . . . . 
+        . . . . . . . e 4 . . . . . . . 
+        . . . . . . e e 5 2 . . . . . . 
+        . . . . . . e 4 5 2 . . . . . . 
+        . . . . . c c c 2 2 2 . . . . . 
+        . . . . e e 4 4 4 5 2 2 . . . . 
+        . . e f f f c c 2 2 f f 2 2 . . 
+        . e e e e 2 2 4 4 4 4 5 4 2 2 . 
+        e e e e e e 2 2 4 4 4 5 4 4 2 2 
+        e e e e e e 2 2 4 4 4 4 5 4 2 2 
+        `, mySprite, 100, -200)
+    mySprite.ay = Gravity
+})
+let projectile: Sprite = null
